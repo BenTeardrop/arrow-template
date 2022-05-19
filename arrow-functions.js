8f7199c41f5b58ee18ccd1bd063ad11a09c725a8
Ben Jim <jimenezbenjamin@yahoo.com>
2022-05-19 13:58:35 +0000

    Add different arrow functions examples

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,34 @@
 
 // Vanilla JavaScript Function
 
+//  
+
 // Arrow Function With Parameters
 
+const addNumbers = (a, b) => {
+    // Code Block 
+    return a + b
+}
+
+let sum = addNumbers(3, 4)
+console.log (sum) 
+
 // Single Line Arrow Function With Parameters
+// const addNumbers2 = (a, b) => (a + b); also valid
+const addNumbers2 = (a, b) => a + b;
+let sum2 = addNumbers2(5, 5);
+console.log (sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello mates')
 
+const sayHello = () => console.log ('hello')
+sayHello()
 // Returning Multiple Lines
+const multipleLines = () => (
+    `<p>
+        this is a multiple line string
+    </p>`
+)
+console.log (multipleLines())
